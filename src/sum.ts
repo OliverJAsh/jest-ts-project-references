03327b2d8567ffa142f9020a2188f114916f2867
Oliver Joseph Ash <oliverjash@gmail.com>
2018-09-26 14:15:38 +0100

    Add bug example

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,8 @@
+// Uncomment this line:
+// test('foo', () => {})
+// Expected: error in VSCode and when running `tsc -b`
+// Actual: no error in VSCode, error when running `tsc -b`
+
 export function sum(a: number, b: number) {
   return a + b;
 }

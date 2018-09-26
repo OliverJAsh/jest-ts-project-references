63990850e6a344a5d8a54562257d8e374cc20f57
Oliver Joseph Ash <oliverjash@gmail.com>
2018-09-26 17:08:31 +0100

    Initial

--- src/sum.ts
+++
@@ -1,8 +0,0 @@
-// Uncomment this line:
-// test('foo', () => {})
-// Expected: error in VSCode and when running `tsc -b`
-// Actual: no error in VSCode, error when running `tsc -b`
-
-export function sum(a: number, b: number) {
-  return a + b;
-}

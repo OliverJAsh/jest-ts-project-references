ac25a2d857cae3831d5aff35dd32776f1e47f4a6
Oliver Joseph Ash <oliverjash@gmail.com>
2018-09-27 07:33:45 +0100

    Test other compiler options

--- src/__tests__/add.ts
+++ src/__tests__/add.ts
@@ -1,5 +1,9 @@
 import { add } from '../add';
 
+// This should error because `tsconfig-tests.json` specifies `compilerOptions` `noUnusedLocals` and
+// `noUnusedParameters`.
+const foo = 1;
+
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
